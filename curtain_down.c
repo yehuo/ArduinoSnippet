b0be5b819c40fc701705cce20e2dbfe2dd3ce41b
Shujia Xu <2497473001@qq.com>
2024-02-03 15:58:32 +0800

    Create curtain_down.c
    Use DS3231 to alert curtain and coffee at 14:00 from Monday to Friday

---
+++ curtain_down.c
@@ -0,0 +1,46 @@
+#include <Wire.h>
+#include "RTClib.h"
+
+RTC_DS3231 rtc;
+
+void setup () {
+  Serial.begin(9600);
+
+  if (!rtc.begin()) {
+    Serial.println("找不到DS3231");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC power lost, RTC module time is reset");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    Serial.println("Time is reset！");
+  }
+}
+
+void loop () {
+  DateTime now = rtc.now();
+
+  if (now.dayOfTheWeek() >= 1 && now.dayOfTheWeek() <= 5 && now.hour() == 14 && now.minute() == 0 && now.second() == 0) {
+    Serial.println("Time to coffee");
+    alert_time(now);
+    open_curtain();
+  }
+  
+  delay(1000);
+}
+
+void open_curtain() {
+  Serial.println("Open Curtain");
+}
+
+void alert_time(DateTime now) {
+  String timeCommand = "Current Time is ";
+  char buffer[20];
+  sprintf(
+    buffer, "%04d/%02d/%02d %02d:%02d:%02d", 
+          now.year(), now.month(), now.day(), 
+          now.hour(), now.minute(), now.second());
+  Serial.print(timeCommand);
+  Serial.println(buffer);
+}
